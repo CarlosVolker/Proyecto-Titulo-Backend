9090cc4a94e199a675cd117677915947f3b1f644
CarlosVolker <carlosvegaalarcon@outlook.com>
2024-12-29 16:58:26 -0300

    Actualización de archivo Settings, ya no tiene las credenciales de la DB

--- ControlPoligono/control_poligono/settings.py
+++ ControlPoligono/control_poligono/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-512-g+f6xglm$tdyqn5)v)*w^5)kj)@qyube1%p+-$t+c7o*n='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ["192.168.1.86", "localhost", "127.0.0.1"]
 
@@ -93,11 +94,11 @@ WSGI_APPLICATION = 'control_poligono.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_poligono',
-        'USER': 'postgres',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',  # O la dirección IP de tu servidor PostgreSQL
-        'PORT': '5432',  # O el puerto que estés usando
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),  # O la dirección IP de tu servidor PostgreSQL
+        'PORT': config('DB_PORT', default='5432'),  # O el puerto que estés usando
     }
 }
 
